eee7d93128ffe245380985f2a83906335fceef20
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-08-20 10:33:04 -0300

    Constructores

--- python_poo.py
+++ python_poo.py
@@ -82,4 +82,33 @@ print('el medico tiene edad?', hasattr(medico, 'edad')) # retorna True
 print('Antes era: ', medico.nombre)
 setattr(medico, 'nombre', 'Marcos') #se llama en comillas para que acceda al valor declarado
 print('Ahora se llama: ', medico.nombre)
-delattr(Persona, 'paiss') #elimina el atributo
+delattr(Persona, 'pais') #elimina el atributo
+
+# CONSTRUCTOR
+
+class Familiar:
+    def __init__(self, nombre1, parentesco):
+        self.nombre1 = nombre1
+        self.parentesco = parentesco
+    def descripcion(self):
+        return ' {} tiene {} años'.format(self.nombre1, self.parentesco)
+
+    def comentario(self, frase):
+        return '{} dice: {}'.format(self.nombre1 , frase)
+
+humano = Familiar('Maria', 23)
+print(humano.descripcion())
+print(humano.comentario('Hola que tal?'))
+
+# MODIFICAR UN ATRIBUTO
+
+class Email:
+    def __init__(self):
+        self.enviado = False
+    def enviar_correo(self):
+        self.enviado = True
+mi_correo = Email()
+
+#print(mi_correo.enviado)
+mi_correo.enviar_correo() #Llamo al otro metodo
+print(mi_correo.enviado)
