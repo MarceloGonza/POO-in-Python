a71e8b18058e8b4beb5245a4f3919369159478f3
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-08-21 10:35:20 -0300

    Ejercicio Herencia

--- python_poo.py
+++ python_poo.py
@@ -134,3 +134,17 @@ class charmander(pokemon):
 nuevo_pokemon = pikachu('pika', 'electrico')
 print(nuevo_pokemon.descripcion())
 print(nuevo_pokemon.ataque('impact trueno'))
+
+# Ejercicio Herencia
+class culturistas:
+    def __init__(self, nombreCulturista, categoria):
+        self.nombreCulturista = nombreCulturista
+        self.categoria = categoria
+    def categoriaComp(self):
+        return '{} es un culturista de la categoria {}'.format(self.nombreCulturista, self.categoria)
+class cBum (culturistas):
+    def peso (self, peso):
+        return 'el culturista {} en competicion pesa: {}'.format(self.nombreCulturista, peso)
+nuevoCulturista = cBum('CBum', 'Classic Physique',)
+print(nuevoCulturista.categoriaComp())
+print(nuevoCulturista.peso('100kg'))
